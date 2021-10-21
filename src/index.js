59186d8b7271f65ebb1a820f5994489b784f2c92
leigp <907090585@qq.com>
2021-10-21 18:03:19 +0800

    update

--- src/index.js
+++ src/index.js
@@ -1,35 +1,50 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-const root=document.querySelector("#root")
-const App=()=>{
+
+const root = document.querySelector("#root")
+const App = () => {
     return (
         <div className="container">
-           <Son />
+            <span>Father</span>
+            <Son/>
         </div>
     )
 }
-class Son extends React.Component{
-    constructor(props){
+
+class Son extends React.Component {
+    constructor(props) {
         super(props)
-        this.state={number:1}
+        this.state = {number: 1}
+
     }
-    add(){
-        const data=()=>{return{n:this.state.number+1}}
-        this.setState(data())
+
+    add = () => {                                   //放在原型上，就可以使用{this.add直接调用}，箭头函数继承原来的this
+        const data = this.state                   //这样可以消除直接修改state的警告
+        this.setState(() => {
+            return {number: data.number += 1}
+        })
     }
-    render(){
+
+    render() {
         return (
             <div>
-                {this.state.number}
-                <button onClick={()=>this.add()}>+1</button>
+                son:{this.state.number}
+                <button onClick={this.add}>+1</button>
+                <hr/>
+                <Grandson/>
             </div>
+            //或者()=>{this.add()}调用
         )
     }
 }
 
-
-
-
-
-
-ReactDOM.render(<App/>,root)
+const Grandson = (props) => {
+    const [n, setState] = React.useState(3)
+    return (
+        <div>
+            <span> grandson:{n}</span>
+            <button onClick={() => setState(n + 1)}>+1</button>
+        </div>
+    )
+}
+ReactDOM.render(<App/>, root)
